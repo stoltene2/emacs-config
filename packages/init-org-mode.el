58769b60b5755dac9c6450f930ac89d0eaf028a9
Eric Stolten <eric.stolten@adp.com>
2015-03-12 10:29:55 -0400

    Insert org-mode configuration

---
+++ packages/init-org-mode.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ '(org-agenda-files
+   (quote
+    ("~/Documents/Redbox/sprints/sprints.org" "~/Documents/Redbox/projects.org")))
+ '(org-clock-clocktable-default-properties (quote (:maxlevel 3 :scope file)))
+ '(org-clock-idle-time 15)
+ '(org-clock-into-drawer "LOGBOOK")
+ '(org-clock-out-remove-zero-time-clocks t)
+ '(org-clocktable-defaults
+   (quote
+    (:maxlevel 3 :lang "en" :scope file :block nil :tstart nil :tend nil :step nil :stepskip0 nil :fileskip0 nil :tags nil :emphasize nil :link nil :narrow 40! :indent t :formula nil :timestamp nil :level nil :tcolumns nil :formatter nil)))
+ '(org-enforce-todo-checkbox-dependencies t)
+ '(org-enforce-todo-dependencies t)
+ '(org-fontify-emphasized-text t)
+ '(org-fontify-whole-heading-line t)
+ '(org-habit-following-days 5)
+ '(org-habit-show-habits-only-for-today t)
+ '(org-habit-today-glyph 124)
+ '(org-hide-emphasis-markers t)
+ '(org-hide-leading-stars t)
+ '(org-log-done (quote time))
+ '(org-modules
+   (org-bibtex org-crypt org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-vm org-wl org-w3m org-mouse))
+ '(org-tags-column -120)
+ '(org-todo-keyword-faces (quote (("TODO" . "#b58900") ("NEXT" . "#2aa198")))))
