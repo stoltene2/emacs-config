a735dccdcfb5e28f794183cf0a3c87c1e8af5869
Eric Stolten <eric.stolten@adp.com>
2015-05-21 14:31:28 -0400

    Sort dired files at top

--- init/20-mac-customizations.el
+++ init/20-mac-customizations.el
@@ -2,23 +2,27 @@
 ;; theme.
 
 (if (eq system-type 'darwin)
-    (progn (setq mac-command-modifier 'meta)
+    (progn
+      (require 'ls-lisp)
+      (setq ls-lisp-use-insert-directory-program nil)
 
-           (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
-           (setq mouse-wheel-progressive-speed nil)            ;; don't accelerate scrolling
-           (setq mouse-wheel-follow-mouse 't)                  ;; scroll window under mouse
-           (setq scroll-step 1)                                ;; keyboard scroll one line at a time
+      (setq mac-command-modifier 'meta)
 
-           ;; The popup message box destroys the system
-           (defadvice yes-or-no-p (around prevent-dialog activate)
-             "Prevent yes-or-no-p from activating a dialog"
-             (let ((use-dialog-box nil))
-               ad-do-it))
-           (defadvice y-or-n-p (around prevent-dialog-yorn activate)
-             "Prevent y-or-n-p from activating a dialog"
-             (let ((use-dialog-box nil))
-               ad-do-it))
-           (defadvice message-box (around prevent-dialog activate)
-             "Prevent message-box from activating a dialog"
-             (apply #'message (ad-get-args 0)))
-           ))
+      (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+      (setq mouse-wheel-progressive-speed nil)            ;; don't accelerate scrolling
+      (setq mouse-wheel-follow-mouse 't)                  ;; scroll window under mouse
+      (setq scroll-step 1)                                ;; keyboard scroll one line at a time
+
+      ;; The popup message box destroys the system
+      (defadvice yes-or-no-p (around prevent-dialog activate)
+        "Prevent yes-or-no-p from activating a dialog"
+        (let ((use-dialog-box nil))
+          ad-do-it))
+      (defadvice y-or-n-p (around prevent-dialog-yorn activate)
+        "Prevent y-or-n-p from activating a dialog"
+        (let ((use-dialog-box nil))
+          ad-do-it))
+      (defadvice message-box (around prevent-dialog activate)
+        "Prevent message-box from activating a dialog"
+        (apply #'message (ad-get-args 0)))
+      ))
