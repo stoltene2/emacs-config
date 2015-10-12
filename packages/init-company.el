06f2dede3cd8695c67f066dcce5329f178e3713f
Eric Stolten <eric.stolten@adp.com>
2015-10-12 07:19:53 -0400

    Set company prefix length to 3
    It was set to two and it was really annoying in Haskell Mode.

--- packages/init-company.el
+++ packages/init-company.el
@@ -2,7 +2,7 @@
           (lambda ()
             (progn
               (global-company-mode)
-              (setq company-minimum-prefix-length 2)
+              (setq company-minimum-prefix-length 3)
               (setq company-tooltip-margin 1)
               (setq company-tooltip-minimum-width 30)
               (global-auto-complete-mode -1))))
